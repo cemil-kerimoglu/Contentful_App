6864ca8efbadb6e169c503d381663a0bd06ba3d3
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2022-08-04 20:29:56 +0200

    Revert "Anselms contentful"

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { createClient } from "contentful";
-import { useState, useEffect } from "react";
-
-const { REACT_APP_CONTENTFUL_API_KEY, REACT_APP_CONTENTFUL_SPACE_ID } =
-  process.env;
-
-const client = createClient({
-  space: `${REACT_APP_CONTENTFUL_SPACE_ID}`,
-  accessToken: `${REACT_APP_CONTENTFUL_API_KEY}`,
-});
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    client.getEntry("4WkmpGAZvKgBh99KvI96cp").then(function (entry) {
-      // logs the entry metadata
-      console.log(entry.fields);
-      setData(entry.fields);
-      // logs the field with ID title
-      console.log(entry.fields.title);
-      console.log(entry.fields.mainPicture.fields.file.url);
-      console.log(entry.fields.mainPicture.fields.description);
-    });
-  }, []);
-
   return (
     <div className="App">
-      <p> Hello there, contentful </p>{" "}
-      <p> api key: {REACT_APP_CONTENTFUL_API_KEY} </p>{" "}
-      <p> space id: {REACT_APP_CONTENTFUL_SPACE_ID} </p>{" "}
-      <p>
-        {" "}
-        "{data?.title}", written by <strong>{data?.author}</strong> published on{" "}
-        {data?.publishDate}
-      </p>
-      {`https:${data?.mainPicture?.fields.file.url}`}
-      <img
-        src={`https:${data?.mainPicture?.fields.file.url}`}
-        alt="no pic 4 u"
-      />
-      <p>{data?.mainPicture?.fields.description}</p>
-      <p> Text:"{data?.content?.content[0].content[0].value}" </p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
